bc02183b31a4ee2f04185467dfc1bc157052eccd
Marat Khafizov <xafizoff@gmail.com>
2018-12-01 00:15:16 +0300

    Watch only create/modify/delete events

--- watch.sh
+++ watch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while true; do
   mlton -output n2o.exe n2o.mlb
-  inotifywait --exclude '\.\#.*$' ./src n2o.mlb
+  inotifywait -ecreate -edelete -emodify --exclude '\.\#.*$' -q ./src n2o.mlb
 done 
